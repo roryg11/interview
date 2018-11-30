5d3b5f5893c4e80ccb33cb67a73a116f1584fc57
Rory Grant <Rory.C.Grant@gmail.com>
2018-11-30 08:08:20 -0800

    fixing to use instance variables instead of pure functionality

--- lib/address.rb
+++ lib/address.rb
@@ -3,17 +3,17 @@ require_relative 'geocoding'
 class Address
   attr_accessor :lat, :lng, :full_address
 
-  def reverse_geocoded_by(lat, lng)
-    Geocoder.search([lat, lng]).first.address
+  def reverse_geocoded_by
+    Geocoder.search([@lat, @lng]).first.address
   end
 
-  def get_coordinates(address)
-    Geocoder.search(address)
+  def get_coordinates
+    Geocoder.search(@full_address)
   end
 
   def get_distance_from(addressTo, address)
     addressToCoordinates = Geocoder.search(addressTo).first.coordinates
-    addressFromCoordinates = Geocoder.search(address.full_address);
+    addressFromCoordinates = Geocoder.search(@address.full_address);
     addressFromCoordinates.distance_to([addressTo.latitude, addressTo.longitude])
   end
 end
